e28c0f9e03c956af140824899519add683130007
PhuthiKat <108768362+PhuthiKat@users.noreply.github.com>
2023-07-30 13:11:48 +0200

    Add files via upload

---
+++ NameSurnameFrameApp.java
@@ -0,0 +1,15 @@
+
+package namesurnameframeapp;
+
+import za.ac.tut.ui.NameSurnameFrame;
+
+public class NameSurnameFrameApp {
+
+    public static void main(String[] args)
+    {
+      NameSurnameFrame namesurname;
+      
+      namesurname = new NameSurnameFrame();
+    }
+    
+}
